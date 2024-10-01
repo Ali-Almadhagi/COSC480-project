a41e7d714ca894457a9d5b48e077ad572ce9fb9d
Ziad Sabri <zsabri@emich.edu>
2024-09-30 22:20:00 -0400

    updated teh templates and the app.py

--- back-end/app.py
+++ back-end/app.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='../front-end/templates')
+app = Flask(__name__, template_folder='../front-end/templates', static_folder='../front-end/static')
 
 @app.route('/')
 def home():
     # Renders the index.html template located in the templates folder
     return render_template('index.html')
+
+@app.route('/detection')
+def detection():
+    return render_template('detection.html')
+
+@app.route('/team')
+def team():
+    return render_template('team.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
